e07d771ccf6cb604fe77b935279be0ee5a663339
kausikaaa <kauss0806@gmail.com>
2025-05-12 19:22:52 +0530

    fet: new file added

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+diabetes_df=pd.read_csv('data/diabetes.csv')
+print(diabetes_df.head())
 
